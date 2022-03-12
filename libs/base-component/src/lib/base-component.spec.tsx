9ececbcb744d1ee6559a72c3b33d6a1b47c5324a
David Ren <david.ren@afterpay.com>
2022-03-12 22:41:50 +0800

    Add conventional commit semver.

--- libs/base-component/src/lib/base-component.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { getBaseComponentString } from './base-component';
-
-describe('baseComponent', () => {
-  it('should work', () => {
-    expect(getBaseComponentString()).toEqual('This is base component');
-  });
-});
