8b0a95fb7359affa3c42f592be1cea6580d78c35
David Ren <david.ren@afterpay.com>
2022-03-15 09:41:30 +0800

    feat: add width

--- libs/base-button-library/src/lib/base-button.tsx
+++ libs/base-button-library/src/lib/base-button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  width: 520px;
+  width: 530px;
   height: 40px;
   background-color: #cccccc;
 `;
